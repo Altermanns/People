a08d22d2a4b9099c55d19dc1b4fa4296e408385d
Altermanns <isaac.277.trujillo@gmail.com>
2024-12-27 16:27:26 -0500

    Almacenamiento de datos de forma local con SQLite

--- People/App.xaml.cs
+++ People/App.xaml.cs
@@ -2,7 +2,16 @@
 
 public partial class App : Application
 {
-    // TODO: Add a public static PersonRepository property
+    public static PersonRepository PersonRepo { get; private set; }
+
+    public App(PersonRepository repo)
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+
+        PersonRepo = repo;
+    }
 
     public App()
 	{
